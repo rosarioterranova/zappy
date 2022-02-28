71c02376e90b4d4b0ca7ab3e001cbdba2aef95d2
Rosario Terranova <rosarioterranova91@gmail.com>
2022-02-28 12:02:09 +0100

    api call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import {useState, useEffect} from "react"
+
 function App() {
+
+  const [propertiesData, setPropertiesData] = useState([])
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/zappyrent/frontend-assessment/properties")
+    .then(res => res.json())
+    .then(data => setPropertiesData(data))
+  }, [])
+  
   return (
     <div className="App">
       <p>Hello World</p>
