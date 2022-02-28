5338acf49f49767307f40b5d98e46fa100e23bbb
Rosario Terranova <rosarioterranova91@gmail.com>
2022-02-28 18:28:20 +0100

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,7 +11,6 @@ export default function Navbar() {
   
   const [filters, setFilters] = useState(ROOM_TYPES.map(type => ({label:type, value:false})))
 
-
   return (
     <div className={style.navbar}>
         <div>
